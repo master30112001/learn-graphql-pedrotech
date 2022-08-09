791f0187a21661168ab36bb053549cffc59cbd8a
Vedant Bothikar <vbothikar2001@gmail.com>
2022-08-10 01:19:04 +0530

    init

---
+++ mydata.js
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    id: 1,
+    name: "vedant",
+    username: "master300",
+    age: 20,
+    nationality: "Indian",
+  },
+  {
+    id: 2,
+    name: "akshit",
+    username: "akmadan",
+    age: 21,
+    nationality: "Pakistani",
+  },
+  {
+    id: 3,
+    name: "ishan",
+    username: "itshan",
+    age: 22,
+    nationality: "Indonesian",
+  },
+  {
+    id: 4,
+    name: "tanuj",
+    username: "pancho",
+    age: 23,
+    nationality: "British",
+  },
+  {
+    id: 5,
+    name: "karan",
+    username: "karwani",
+    age: 24,
+    nationality: "Canadian",
+  },
+];
